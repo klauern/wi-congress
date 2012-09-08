a7d28e7001e7257a07ac1b70252b2b5c052a1390
Nick Klauer <klauer@gmail.com>
2012-09-08 12:52:06 -0500

    remove datomic

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject wi-senate "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject wi-senate "1.0.0-SNAPSHOT"
+  :description "project to show and dig in to the Wisconsin Legislature's RSS Feeds on bills and motions"
+  :url "https://www.github.com/klauern/wi-congress"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.5.3"]
                  [org.clojure/data.xml "0.0.6"]
                  [org.clojure/data.zip "0.1.1"]
-                 [com.datomic/datomic-free "0.8.3470"]
                  [clojure-lanterna "0.9.2"]])
