5b5f532dfc9711aa0a8d5913d6e4d1b1d1e5b986
Nick Klauer <klauer@gmail.com>
2012-09-10 22:37:35 -0500

    shouldn't have to parse multiple times.

--- test/wi_congress/test/rss.clj
+++ test/wi_congress/test/rss.clj
@@ -15,9 +15,9 @@
 (fact "retrieving an rss feed returns a zipper"
       (type rss-zipper) => clojure.lang.PersistentVector)
 
-(defonce floor "floor.rss")
+(def floor (rss-zipify-file "floor.rss"))
 
 (fact "can retrieve a list of rss items from file"
-      (type (rss-zipify-file floor)) => clojure.lang.PersistentVector
-      (count (take 5 (get-items (rss-zipify-file floor)))) => 5
-      (xml/xml1-> (nth (get-items (rss-zipify-file floor)) 2) :guid xml/text)=> "0e30f9f8-b58e-44b8-9239-9ac775f900ba")
+      (type floor) => clojure.lang.PersistentVector
+      (count (take 5 (get-items floor))) => 5
+      (xml/xml1-> (nth (get-items floor) 2) :guid xml/text)=> "0e30f9f8-b58e-44b8-9239-9ac775f900ba")
