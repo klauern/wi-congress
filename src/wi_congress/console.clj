90155c93899c12da9a03de800bbe92c349208c50
Nick Klauer <klauer@gmail.com>
2012-09-11 22:08:11 -0500

    tweaks
    Tweaks to the last commit

--- src/wi_congress/console.clj
+++ src/wi_congress/console.clj
@@ -18,11 +18,10 @@
         link        (xml/xml1-> item :link xml/text)
         title       (xml/xml1-> item :title xml/text)
         description (xml/xml1-> item :description xml/text)
-    (t/put-string "Feed " guid 
         pubdate     (xml/xml1-> item :pubdate xml/text)
+        updated     (xml/xml1-> item :a10:updated xml/text)]
     (t/put-string term (str "Feed " guid 
              " on " pubdate 
-             "\nTitle: " title
              " last updated on " updated))
     (t/put-string term link)
     (t/put-string term (str "Title: " title))
