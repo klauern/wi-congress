4322988568fb203ac26e4579051bae624e9595e7
Nick Klauer <klauer@gmail.com>
2012-09-11 22:06:54 -0500

    add some tests
    Nothing fancy.  Not sure I even know what I'll test from the swing
    console-side of the house, but I need to at least know how to stub out
    the RSS var and terminal.

---
+++ test/wi_congress/test/console.clj
@@ -0,0 +1,16 @@
+(ns wi-congress.test.console
+  (:use midje.sweet
+        wi-congress.console
+        [wi-congress.rss :only (rss-zipify-file get-items)])
+  (:require [lanterna.terminal :as t]))
+
+(def rss-items (-> "floor.rss"
+                 rss-zipify-file
+                 get-items))
+
+(def second-one (nth rss-items 1))
+
+(fact "can print a feed item to console"
+      (console-print-feed-item second-one) => nil
+      @screen-size => [142 50])
+
